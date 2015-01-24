aa56f54311afd8ccdc91a5500bb1f312ec15e06b
saianudeepm <sai.anudeep.m@gmail.com>
2015-01-24 05:45:46 -0800

    Initial Commit

---
+++ RottenMovies/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RottenMovies
+//
+//  Created by Sai Anudeep Machavarapu on 1/24/15.
+//  Copyright (c) 2015 salome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RottenMovies/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RottenMovies
+//
+//  Created by Sai Anudeep Machavarapu on 1/24/15.
+//  Copyright (c) 2015 salome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
